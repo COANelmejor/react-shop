2682c91096642f85c4d245547b0ec9b2207ca5d7
Christian Obed Arana navas <COANelmejor@gmail.com>
2022-05-15 01:29:13 -0600

    Corrected Warning from webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-import React from 'React';
+import React from 'react';
 
 const App = () => {
     return (
-        <h1>Hola Mundo</h1>
+        <h1>Hola Mundo, Bienvenido al curso práctico de React!</h1>
     );
 };
 
