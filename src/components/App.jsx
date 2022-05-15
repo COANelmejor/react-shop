c84121e0dfbbba6dc946ca82238685c3e150e8e7
Christian Obed Arana navas <COANelmejor@gmail.com>
2022-05-15 10:51:53 -0600

    8/29 Estilos en los componentes de React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import '../styles/global.scss';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+
+import '../styles/global.css';
+
 
 const App = () => {
     return (
-        <h1>Hola Mundo, Bienvenido al curso práctico de React!</h1>
+        <Layout>
+            <Login />
+        </Layout>
     );
 };
 
