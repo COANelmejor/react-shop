a05855760ee664cb45a5352a5f8b4982784abe22
Christian Obed Arana navas <COANelmejor@gmail.com>
2022-05-16 23:27:03 -0600

    13/29 Solución al reto: componentes y rutas de React Shop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import Header from '../components/Header';
+import ProductList from '../containers/ProductList';
 
 const Home = () => {
-    return ( 
+    return (
         <>
-            <Header />
+            <ProductList />
         </>
     );
 }
 
 export default Home;
-
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-// TODO Create a great design for this page
 const NotFound = () => {
     return (
         <div>
-            <h1>404</h1>
-            <p>Page not found</p>
+            No encontrado, error 404.
         </div>
     );
 }
