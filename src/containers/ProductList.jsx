99d7c0be165e4c968a27102808ef4ae238bacef6
Christian Obed Arana navas <COANelmejor@gmail.com>
2022-05-23 00:09:04 -0600

    18/29 useEffect y consumo de APIs

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
 
+const API = 'https://api.escuelajs.co/api/v1/products'
+
 const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(async () => {
+        const response = await fetch(API);
+        fetch(API)
+            .then(response => response.json())
+            .then(data => setProducts(data));
+        // const response = await axios.get(API);
+        // setProducts(response.data);
+    }, []);
+
     return (
         <section className="main-container">
             <div className="ProductList">
-                { Array(10).fill(null).map((item, index) => <ProductItem key={index} />) }
+                {
+                    products.map(product => (
+                        <ProductItem />
+                    ))
+                }
             </div>
         </section>
     );
